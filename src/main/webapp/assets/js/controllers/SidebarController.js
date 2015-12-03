59cd7b11e63ca0edcb95e129cfc3c2a792dc66c4
Arnold Palar <arnold.palar@gmail.com>
2015-12-03 15:36:37 +0800

    forgot the alert

--- src/main/webapp/assets/js/controllers/SidebarController.js
+++ src/main/webapp/assets/js/controllers/SidebarController.js
@@ -3,7 +3,6 @@
 angular.module('mainApp')
     .controller('SidebarController', function ($scope) {
         $scope.setActive = function($event){
-            alert('qwe');
             jQuery('.side-nav a').removeClass('active');
             jQuery($event.currentTarget).addClass('active');
         }
