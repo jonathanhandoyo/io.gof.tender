3cc61d0ad209dedad99f33317e90ca3370291688
jonathanhandoyo <jonathan.handoyo@gmail.com>
2015-12-04 23:34:38 +0800

    ayo kita keon

--- src/main/java/io/gof/tender/repository/ProjectRepository.java
+++ src/main/java/io/gof/tender/repository/ProjectRepository.java
@@ -1,6 +1,7 @@
 package io.gof.tender.repository;
 
 import io.gof.tender.domain.Project;
+import io.gof.tender.domain.Vendor;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.GraphRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,10 @@ public interface ProjectRepository extends GraphRepository<Project> {
             " WHERE project.code = {0} " +
             "RETURN project; ")
     Project findByCode(String code);
+
+    @Query( " MATCH (project:Project), (vendor:Vendor) " +
+            " WHERE id(project) = {0} " +
+            "   AND id(vendor)  = {1} " +
+            "CREATE UNIQUE (project) -[:APPOINTED_TO]-> (vendor); ")
+    Project setWinner(Project project, Vendor vendor);
 }
--- src/test/java/io/gof/tender/test/ProjectBootstrapper.java
+++ src/test/java/io/gof/tender/test/ProjectBootstrapper.java
@@ -47,8 +47,8 @@ import static org.apache.commons.lang3.tuple.Pair.of;
 @SpringApplicationConfiguration(classes = Application.class)
 @ContextConfiguration(classes = ApplicationConfiguration.class)
 @WebAppConfiguration
-public class DataBootstrapper {
-    private static final Logger LOG = LoggerFactory.getLogger(MainTest.class);
+public class ProjectBootstrapper {
+    private static final Logger LOG = LoggerFactory.getLogger(ProjectBootstrapper.class);
     private static final SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd");
 
     @Autowired
@@ -81,8 +81,8 @@ public class DataBootstrapper {
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withHeader().parse(in);
         for (CSVRecord record : records) {
             try {
-
                 Project project = new Project(
+                        record.get("kode_lelang"),
                         record.get("nama_lelang"),
                         Project.Status.COMPLETE,
                         record.get("kategori"),
@@ -132,17 +132,10 @@ public class DataBootstrapper {
                         null,
                         null
                 );
-                System.out.println(project);
+                System.out.println(this.projects.save(project).getCode());
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-    }
-
-    public static void main(String[] args) throws Exception {
-        String seed = "* Ijin Usaha Ijin Usaha Klasifikasi * Telah melunasi kewajiban pajak tahun terakhir (SPT/PPh) memiliki Nomor Pokok Wajib Pajak (NPWP) dan telah memenuhi kewajiban perpajakan tahun terakhir (SPT Tahunan) serta memiliki laporan bulanan PPh Pasal 21, PPh Pasal 23 (bila ada transaksi), PPh Pasal 25/Pasal 29 dan PPN (bagi Pengusaha Kena Pajak) paling kurang 3 (tiga) bulan terakhir, yaitu Agustus 2013, September 2013 dan Oktober 2013. Peserta dapat mengganti persyaratan ini dengan menyampaikan Surat Keterangan Fiskal (SKF) yang dikeluarkan oleh Kantor Pelayanan Pajak dengan tanggal penerbitan paling lama 1 (satu) bulan sebelum tanggal mulai pemasukan Dokumen Kualifikasi * memiliki pengalaman dalam penyediaan sistem dan infrastruktur Call Center beserta maintenance minimal 3 tahun * memiliki Tenaga Teknis/Terampil untuk melaksanakan pekerjaan ini * memiliki kemampuan untuk menyediakan fasilitas/peralatan/perlengkapan untuk melaksanakan pekerjaan ini";
-//        Arrays.asList(StringUtils.split(seed, "*")).stream().forEach(it -> System.out.println(" - " + it.trim()));
-        String[] test = Arrays.stream(StringUtils.split(seed, "*")).filter(it -> !"Ijin Usaha Ijin Usaha Klasifikasi".equals(it.trim())).toArray(String[]::new);
-        System.out.println(test);
     }
 }
