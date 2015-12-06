29c9af0286c060a2fde71dbb8f541699448fa5f7
Wira Siwananda <wira.siwananda@hrboss.com>
2015-12-06 10:12:39 +0800

    pageable on exist location, put everything to 10

--- src/main/java/io/gof/tender/repository/ProjectRepository.java
+++ src/main/java/io/gof/tender/repository/ProjectRepository.java
@@ -7,12 +7,13 @@ import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, String> {
-    @Query("{location: {$exists:true}}")
-    Stream<Project> findAllWithLocationExists();
+    @Query("{location: {$exists:true}} ")
+    Page<Project> findAllWithLocationExists(Pageable pageable);
 
     @Query("{location: {$exists:false}}")
     Page<Project> findWithoutLocation(Pageable pageable);
